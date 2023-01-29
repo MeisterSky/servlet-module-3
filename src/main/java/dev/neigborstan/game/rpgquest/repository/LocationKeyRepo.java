f0cf8736317de457fe2b1f77d1d3d6c1e2fd8b56
Sheff Victor <sheff.victor@gmail.com>
2023-01-29 12:09:28 +0300

    Creation of beauty

--- src/main/java/dev/neigborstan/game/rpgquest/repository/LocationKeyRepo.java
+++
@@ -1,6 +0,0 @@
-package dev.neigborstan.game.rpgquest.repository;
-
-import dev.neigborstan.game.rpgquest.entity.LocationKey;
-
-public class LocationKeyRepo extends Repository<Integer, LocationKey>{
-}
--- src/main/java/dev/neigborstan/game/rpgquest/repository/QuestionRepo.java
+++
@@ -1,6 +0,0 @@
-package dev.neigborstan.game.rpgquest.repository;
-
-import dev.neigborstan.game.rpgquest.entity.Question;
-
-public class QuestionRepo extends Repository<Integer, Question>{
-}
